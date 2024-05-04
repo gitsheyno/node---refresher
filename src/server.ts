c72ca7d9997b50e597f6c825a013659f1bebb996
shayan.heidary <shayan.heidary@knime.com>
2024-05-04 19:15:26 +0200

    api init - db init

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,21 @@
-import { express } from "express";
+import express from "express";
+// const express = require("express");
+import morgan from "morgan";
+import router from "./router";
+import { protect } from "./modules/auth";
+
+/**
+ * App
+ */
 const app = express();
+
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extends: true }));
 
 /**
  * Our API
  */
-app.get("/", (req, res) => {
-  console.log("hello from express");
-  res.status(200);
-  res.json({
-    message: "Hello",
-  });
-});
+app.use("/api", protect, router);
 
 export default app;
