af672f1c55f5e5b06f030f4fbd482b46945354d7
shayan.heidary <shayan.heidary@knime.com>
2024-05-05 16:05:45 +0200

    rute handlers validation

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-
+import { body, oneOf } from "express-validator";
+import { handleInputsErrors } from "./modules/middlewres";
 const router = Router();
 
 /**
@@ -11,20 +12,28 @@ router.get("/product", (req, res) => {
 });
 
 router.get("/product/:id", (req, res) => {});
-router.post("/product/:id", (req, res) => {});
-router.put("/product/:id", (req, res) => {});
+router.post("/product/:id",  body('name').isString(),handleInputsErrors,(req, res) => {});
+router.put("/product/:id", body('name').isString(),handleInputsErrors,(req, res) => {});
 router.delete("/product/:id", (req, res) => {});
 
 /**
  * Update
- */
+ */ 
 router.get("/update", (req, res) => {});
 
 router.get("/update/:id", (req, res) => {});
 
-router.post("/update", (req, res) => {});
+router.post("/update", 
+  body('title').exists(),
+  body('body').exists(), 
+   (req, res) => {});
 
-router.put("/update/:id", (req, res) => {});
+router.put("/update/:id",
+  body('title').optional(),
+  body('body').optional (),
+  body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
+  body("version").optional(),
+  (req, res) => {});
 
 router.delete("/update/:id", (req, res) => {});
 
@@ -38,9 +47,15 @@ router.get("/updatepoint/:id", (req, res) => {});
 
 router.post("/updatepoint", (req, res) => {});
 
-router.put("/updatepoint/:id", (req, res) => {});
+router.put("/updatepoint/:id",
+body('name').optional().isString(), 
+body('description').optional().isString(),
+(req, res) => {});
 
-router.delete("/updatepoint/:id", (req, res) => {});
+router.delete("/updatepoint/:id",
+body('name').isString(), 
+body('description').isString(),
+body('updateId').exists().isString(),(req, res) => {});
 
 console.log("hello", process.env.JWT_SECRET);
 
