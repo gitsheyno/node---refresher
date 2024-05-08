a8d5e2ac140d1d3017e24fef2cd1eb6b1a389ffe
shayan.heidary <shayan.heidary@knime.com>
2024-05-08 14:44:05 +0200

    new migrate for db - handle create - delete - update product

--- src/handlers/products.ts
+++ src/handlers/products.ts
@@ -21,7 +21,7 @@ const getProducts = async (req, res) => {
 const getOneProduct = async (req, res) => {
   const id = req.params.id;
 
-  const product = await prisma.product.findUnique({
+  const product = await prisma.product.findFirst({
     where: {
       id,
       belongsToId: req.user.id,
@@ -29,4 +29,54 @@ const getOneProduct = async (req, res) => {
   });
 
   res.json({ data: product });
+};
+
+/**
+ * create Product
+ */
+const createProduct = async (req, res) => {
+  const product = await prisma.product.create({
+    data: {
+      name: req.body.name,
+      belongsToId: req.user.id,
+    },
+  });
+
+  res.json({ data: product });
+};
+
+/**
+ * update Product
+ */
+const updateProduct = async (req, res) => {
+  const updated = await prisma.product.update({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id,
+      },
+    },
+    data: {
+      name: req.body.name,
+    },
+  });
+
+  res.json({ data: updated });
+};
+
+/**
+ * delete Product
+ */
+
+const deleteProduct = async (req, res) => {
+  const deleted = await prisma.product.delete({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id,
+      },
+    },
+  });
+
+  res.json({ data: deleted });
 };
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { body, oneOf } from "express-validator";
-import { handleInputsErrors } from "./modules/middlewres";
+import { handleInputsErrors } from "./modules/middlewares";
 const router = Router();
 
 /**
@@ -12,28 +12,42 @@ router.get("/product", (req, res) => {
 });
 
 router.get("/product/:id", (req, res) => {});
-router.post("/product/:id",  body('name').isString(),handleInputsErrors,(req, res) => {});
-router.put("/product/:id", body('name').isString(),handleInputsErrors,(req, res) => {});
+router.post(
+  "/product/",
+  body("name").isString(),
+  handleInputsErrors,
+  (req, res) => {}
+);
+router.put(
+  "/product/:id",
+  body("name").isString(),
+  handleInputsErrors,
+  (req, res) => {}
+);
 router.delete("/product/:id", (req, res) => {});
 
 /**
  * Update
- */ 
+ */
 router.get("/update", (req, res) => {});
 
 router.get("/update/:id", (req, res) => {});
 
-router.post("/update", 
-  body('title').exists(),
-  body('body').exists(), 
-   (req, res) => {});
+router.post(
+  "/update",
+  body("title").exists(),
+  body("body").exists(),
+  (req, res) => {}
+);
 
-router.put("/update/:id",
-  body('title').optional(),
-  body('body').optional (),
-  body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
+router.put(
+  "/update/:id",
+  body("title").optional(),
+  body("body").optional(),
+  body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]).optional(),
   body("version").optional(),
-  (req, res) => {});
+  (req, res) => {}
+);
 
 router.delete("/update/:id", (req, res) => {});
 
@@ -47,15 +61,20 @@ router.get("/updatepoint/:id", (req, res) => {});
 
 router.post("/updatepoint", (req, res) => {});
 
-router.put("/updatepoint/:id",
-body('name').optional().isString(), 
-body('description').optional().isString(),
-(req, res) => {});
+router.put(
+  "/updatepoint/:id",
+  body("name").optional().isString(),
+  body("description").optional().isString(),
+  (req, res) => {}
+);
 
-router.delete("/updatepoint/:id",
-body('name').isString(), 
-body('description').isString(),
-body('updateId').exists().isString(),(req, res) => {});
+router.delete(
+  "/updatepoint/:id",
+  body("name").isString(),
+  body("description").isString(),
+  body("updateId").exists().isString(),
+  (req, res) => {}
+);
 
 console.log("hello", process.env.JWT_SECRET);
 
